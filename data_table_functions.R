426e86e18c5e77561fc6b025d858f0c146463adb
dratnadiwakara <dratnadiwakara2@lsu.edu>
2024-10-23 09:04:05 -0400

    data table functions

--- data_table_functions.R
+++ data_table_functions.R
@@ -6,16 +6,7 @@ expand_list_columns <- function(dt) {
   list_cols <- sapply(dt, is.list)
   
   # For each list column, expand it into separate rows
-  dt_expanded <- dt[, lapply(.SD, function(x) unlist(x, recursive = FALSE)), .SDcols = list_cols]
-  
-  # Identify non-list columns
-  non_list_cols <- dt[, !list_cols, with = FALSE]
-  
-  # Repeat the non-list columns to match the expanded rows from the list columns
-  repeated_non_list_cols <- non_list_cols[rep(1:.N, sapply(dt[[which(list_cols)[1]]], length))]
-  
-  # Combine the repeated non-list columns with the expanded list columns
-  result <- cbind(repeated_non_list_cols, dt_expanded)
+  result <- dt[, lapply(.SD, function(x) unlist(x, recursive = FALSE)), .SDcols = list_cols]
   
   return(result)
 }
