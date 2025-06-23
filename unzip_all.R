6e748b0039a0a6ba4c7c6cd0ec454dae4e9dd6e9
dratnadiwakara <dratnadiwakara2@lsu.edu>
2025-06-23 15:58:46 -0400

    unzip-fast

--- unzip_all.R
+++ unzip_all.R
@@ -28,5 +28,26 @@ unzip_all <- function(directory) {
   cat("All zip files (including nested) have been extracted.\n")
 }
 
+unzip_all_fast <- function(directory) {
+  zip_queue <- list.files(directory, pattern = "\\.zip$", recursive = TRUE, full.names = TRUE)
+  
+  while (length(zip_queue) > 0) {
+    zip_file <- zip_queue[[1]]
+    zip_queue <- zip_queue[-1]
+    
+    extract_path <- file.path(dirname(zip_file), tools::file_path_sans_ext(basename(zip_file)))
+    dir.create(extract_path, showWarnings = FALSE)
+    
+    # Use system unzip (much faster than R's unzip())
+    system2("unzip", args = c("-q", shQuote(zip_file), "-d", shQuote(extract_path)))
+    
+    # Discover any new .zip files
+    new_zips <- list.files(extract_path, pattern = "\\.zip$", recursive = TRUE, full.names = TRUE)
+    zip_queue <- c(zip_queue, new_zips)
+  }
+  
+  cat("All zip files extracted using system unzip.\n")
+}
+
 # Usage
 # unzip_all("path/to/your/folder")
